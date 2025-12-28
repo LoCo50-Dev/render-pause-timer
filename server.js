b6bbf1897b50035764bc162ebda15219b3275217
LoCo50-Dev <co.s.j.z.mann@gmail.com>
2025-12-28 14:48:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+app.use(express.json());
+app.use(express.static("public"));
+
+const STATE_FILE = "./state.json";
+
+function saveState(data){
+  fs.writeFileSync(STATE_FILE, JSON.stringify(data));
+}
+
+function loadState(){
+  if (!fs.existsSync(STATE_FILE)) return {};
+  return JSON.parse(fs.readFileSync(STATE_FILE));
+}
+
+app.get("/state", (req, res) => {
+  res.json(loadState());
+});
+
+app.post("/start", (req, res) => {
+  const mins = req.body.minutes;
+  const totalMs = mins * 60000;
+  const endTime = Date.now() + totalMs;
+
+  saveState({ totalMs, endTime });
+  res.json({ ok:true });
+});
+
+app.listen(process.env.PORT || 3000);
 
